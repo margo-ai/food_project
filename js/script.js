eee89c32d66b96028964d5edbc978971f961af7f
Margo_Yuukoo <plokhova.margarita@yandex.ru>
2020-10-24 20:14:07 +0300

    Лекция 43 Создаем модальное окно

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
     const tabsParent = document.querySelector('.tabheader__items');
@@ -38,4 +39,113 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     
+    // Timer
+
+    const deadline = '2020-12-31';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()); // разница в миллисекундах
+        const days = Math.floor(t / (1000 * 60 * 60 * 24)); // сколько суток осталось до окончания дедлайна
+        const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        const minutes = Math.floor((t / 1000 / 60) % 60);
+        const seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector('#days');
+        const hours = timer.querySelector('#hours');
+        const minutes = timer.querySelector('#minutes');
+        const seconds = timer.querySelector('#seconds');
+        const timeInterval = setInterval(updateClock, 1000);
+
+        updateClock(); // чтобы таймер сразц запустился и мы не ждали секунду, иначе там сперва время из верстки
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]');
+    const modal = document.querySelector('.modal');
+    const modalCloseBtn = document.querySelector('[data-close]');
+
+    // вариант через toggle:
+
+    // modalTrigger.addEventListener('click', () => {
+    //     // modal.classList.add('show');
+    //     // modal.classList.remove('hide');
+    //     modal.classList.toggle('show');
+    //     document.body.style.overflow = 'hidden';
+    // });
+
+    // modalCloseBtn.addEventListener('click', () => {
+    //     // modal.classList.add('hide');
+    //     // modal.classList.remove('show');
+    //     modal.classList.toggle('show');
+    //     document.body.style.overflow = '';
+    // });
+
+
+    modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    
+    modalCloseBtn.addEventListener('click', closeModal);
+
+        
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    // Если участок кода повторяется хотя бы 2 раза, то лучше вынести его в отдельную функцию (принцип DRY)
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
