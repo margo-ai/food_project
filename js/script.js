400a257edcaef729823b366213b6f18c7b01f61b
MargoYuukoo <plokhova.margarita@yandex.ru>
2020-10-28 13:29:39 +0300

    Лекция 59 - сформировала карточки по данных, полученным с json-сервера

--- js/script.js
+++ js/script.js
@@ -207,51 +207,52 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // можно использовать такой синтаксис:
-    // const div = new MenuCard();
-    // div.render();
+    const getResource = async (url) => { // res - это промис
+        const res = await fetch(url);
 
-    // другой способ (мы этот объект нигде не сохраняем как переменную, испоьлзуем один раз):
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-        'menu__item'
-    ).render();
+        // свойства promise, которые скажут, что произошла ошибка при отправке запроса (reject).
+        // т.к. fetch покажет ошибку, только если отсутствует соединение
+        if (!res.ok) { // если результат res не ok
+           throw new Error(`Could not fetch ${url}, status: ${res.status}`); // "выкидываем" ошибку
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        '.menu .container',
-        'menu__item'
-    ).render();
+        return await res.json();
+    };
 
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        '.menu .container',
-        'menu__item'
-    ).render();
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price} )=> {
+                // вызываем конструктор MenuCard
+                // последним аргументом указываем родителя, куда это все помещаем
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+        
 
-    new MenuCard(
-        "img/tabs/balance.jpg",
-        "balance",
-        'Меню "Сбалансированное"',
-        'Меню "Сбалансированное" - это соответствие вашего рациона всем научным рекомендациям. Мы тщательно просчитываем вашу потребность в к/б/ж/у и создаем лучшие блюда для вас.',
-        11,
-        '.menu .container',
-        'menu__item'
-    ).render();
+        // Второй вариант, который не создает классы, а формирует верстку "на лету":
 
+        // getResource('http://localhost:3000/menu')
+        //     .then(data => createCard(data));
+
+        // function createCard(data) {
+        //     data.forEach(({img, altimg, title, descr, price}) => {
+        //         const element = document.createElement('div');
+
+        //         element.classList.add('menu__item');
+        //         element.innerHTML = `
+        //             <img src=${img} alt=${altimg}>
+        //             <h3 class="menu__item-subtitle">${title}</h3>
+        //             <div class="menu__item-descr">${descr}</div>
+        //             <div class="menu__item-divider"></div>
+        //             <div class="menu__item-price">
+        //                 <div class="menu__item-cost">Цена:</div>
+        //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+        //             </div>
+        //         `;
+
+        //         document.querySelector('.menu .container').append(element);
+        //     });
+        // }
 
 
     // Forms
@@ -264,16 +265,29 @@ window.addEventListener('DOMContentLoaded', () => {
         failure: 'Что-то пошло не так...'
     };
 
-    // осталось подвязать под каждую форму функцию postData
+    
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    // Но куда помещать это сообщение?
-    // Очень частая практика: при отправке запроса мы создаём блок на странице,
-    // куда выводим сообщение, картинку и т.д., и чаще всего он добавляется к форме
+    // в аргументах - url и данные, которые будут поститься
+    // async - асинхронный код
+    // await - синхронный код - ставим его еперд теми операциями, которые надо дождаться.
+    // ЭТО ПАРНЫЕ ОПЕРАТОРЫ, ОБА ДОЛЖНЫ ИСПОЛЬЗОВАТЬСЯ 
+    const postData = async (url, data) => { // res - это промис
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
 
-    function postData(form) {
+        return await res.json();
+    };
+
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault(); // отменяем стандартное поведение браузера при submit, т.е. перезагрузку страницы
             // надо всегда ставить такую команду в AJAX-запросах, чтобы не было казусов
@@ -290,21 +304,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            }); 
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+            // entries берет каждое свойство и формирует из него массив
+            // обратный метод - fromEntries
 
-            
-
-           
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data => data.text())
+                       
+            postData('http://localhost:3000/requests', json)            
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);                
