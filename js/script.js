61baaac413414322021e771db3b91e6733158b34
MargoYuukoo <plokhova.margarita@yandex.ru>
2020-10-31 23:47:33 +0300

    Лекция 65 - модифицировала проект регулярными выражениями + в html изменила дату окончания акции

--- js/script.js
+++ js/script.js
@@ -461,12 +461,16 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     }
 
+    function deleteNoteDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
 
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNoteDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNoteDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -491,9 +495,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {            
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNoteDigits(width)* (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNoteDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -518,7 +522,7 @@ window.addEventListener('DOMContentLoaded', () => {
         dot.addEventListener('click', (e) => {
             const slideTo = e.target.getAttribute('data-slide-to');
            slideIndex = slideTo;
-           offset =  +width.slice(0, width.length - 2) * (slideTo - 1);
+           offset =  deleteNoteDigits(width) * (slideTo - 1);
 
            slidesField.style.transform = `translateX(-${offset}px)`;
 
